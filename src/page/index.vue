8833fe4e7429e12370c487da07c93ed35d488615
htm9527 <2652039446@qq.com>
2018-09-01 16:40:49 +0800

    1.新增less语法。 2.router懒加载 3.save->element-ui及全局配置 4.爱美丽化妆品首页开发

---
+++ src/page/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+     <div class="block">
+      <el-carousel height="150px">
+        <el-carousel-item v-for="(item,index) in item" :key="index">
+          <img class='banner' :src="item.url">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <el-row class='seach-product'>
+      <el-col :span='24'>
+        <el-input placeholder="商品搜索：请输入商品关键字" prefix-icon="el-icon-search" v-model="product" ></el-input>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      item:[
+        {
+          id:1,
+          url:'./static/image/1.png'
+        },
+        {
+          id:2,
+          url:'./static/image/2.png'
+        }
+      ],
+      product:null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.banner{
+  width:375px;
+  height:150px;
+}
+.seach-product{
+  margin:10px 15px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import home from '@/components/home/Home'
+
 Vue.use(Router)
 const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-      meta: { title: '黄大河的网站' }
-
+      name: 'index',
+      component: resolve => require(['@/page/index'],resolve),//懒加载
+      meta: { title: '爱美丽化妆品' }
     },
-     {
+    /*  {
       path: '/home',
       name: 'home',
-      component: home,//懒加载,
+      component: resolve => require(['@/components/home/Home'],resolve),
       meta: { title: '首页' }
-    }
+    } */
   ]
 })
 router.beforeEach((to, form, next) => { /*路由变化修改title*/ if (to.meta.title) { document.title = to.meta.title } next(); })
